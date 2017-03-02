69f968f68bdbccf2b60db4e4d06b724d95ae3ade
Emily McMahon <mcmahonimal@gmail.com>
2017-03-01 18:54:07 -0800

    clean up css. team member is changed from 3/row to 1. clean up footer.

--- js/contact_me.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $("input,textarea").not("[type=submit]").jqBootstrapValidation();
-});
-
-
-/*When clicking on Full hide fail/success boxes */
-$('#name').focus(function() {
-    $('#success').html('');
-});
